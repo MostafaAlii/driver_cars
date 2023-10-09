7799ab7be66d8d703cf0bb9b580eaef42ff42bfb
MostafaAlii <mostafa28alii@gmail.com>
2023-10-09 17:42:30 +0200

    finish vehicles-slider section

--- resources/js/main.js
+++ resources/js/main.js
@@ -46,3 +46,29 @@ document.querySelector('.home').onmouseleave = () =>{
         elm.style.transform = `translateX(0px) translateY(0px)`;
     });
 };
+
+// Vehicles Swipper Slider
+var swiper = new Swiper(".vehicles-slider", {
+    slidesPerView: 1,
+    spaceBetween: 2,
+    loop:true,
+    autoplay:{
+        delay: 2500,
+        disableOnInteractiion: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    },
+  });
